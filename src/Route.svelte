56177292bc9b01ddb2bba8d5392b2c7389b9ce5f
SAMEER TARIQ <imsamtar@gmail.com>
2020-06-21 18:16:06 +0500

    Added guards

--- src/Route.svelte
+++ src/Route.svelte
@@ -7,6 +7,7 @@
 
   export let component = undefined;
   export let path = "./";
+  export let guard = undefined;
 
   let uid = parseInt(1000 + Math.random() * 8999);
   let router = getContext("__router");
@@ -21,7 +22,7 @@
 
   onDestroy(function() {
     $router.routes = $router.routes.filter(rout => rout !== route);
-    if (($router.activeRoute === uid)) $router.activeRoute = null;
+    if ($router.activeRoute === uid) $router.activeRoute = null;
   });
 
   function routeChange() {
@@ -33,10 +34,19 @@
     firstTime = false;
   }
 
+  let passedGuards = true;
+  if (typeof guard === "function") {
+    passedGuards = guard();
+  } else if (guard instanceof Array) {
+    passedGuards = guard.reduce(
+      (pass, guard) => (pass = pass && guard()),
+      true
+    );
+  }
   $: routeChange($updateCount);
 </script>
 
-{#if $router.activeRoute === uid}
+{#if $router.activeRoute === uid && passedGuards}
   {#if component}
     <svelte:component this={component} {...matched} />
   {:else}
