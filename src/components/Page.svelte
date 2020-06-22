3cc878702e3c387cee5d497e753b81e7d797522e
SAMEER TARIQ <imsamtar@gmail.com>
2020-06-22 10:10:33 +0500

    Accept middleware, or middlewares prop also accept guard or guards prop to guard a route

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -5,19 +5,27 @@
   export let route = "/";
   export let src = undefined;
   export let guard = [];
+  export let guards = guard instanceof Array ? guard : [guard];
+  export let middleware = [];
+  export let middlewares =
+    typeof middleware === "function" ? [middleware] : middleware;
 
   const router = getContext("$router");
   const page = {};
-  const guards = typeof guard === "function" ? [guard] : guard;
   let context;
 
   onMount(function() {
     $router.routes = [...$router.routes, page];
-    pagejs(route, function(ctx, next) {
-      const pass = guards.reduce((pass, guard) => pass && guard(ctx), true);
-      if (!pass) return next();
-      $router.active = page;
-      context = ctx;
+    pagejs(route, ...middlewares, function(ctx, next) {
+      const pass = guards.reduce(function(pass, guard) {
+        return pass && (typeof guard === "function" ? guard(ctx) : guard);
+      }, true);
+      if (pass) {
+        $router.active = page;
+        context = ctx;
+      } else {
+        $router.active = null;
+      }
     });
   });
   onDestroy(function() {
